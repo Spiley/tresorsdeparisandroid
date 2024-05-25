1aba4bfc62a1c78b1cc80cea0035b8728aaf0b8e
Valentijn <valentijnkeijser@gmail.com>
2024-05-25 17:29:29 +0200

    handle backbutton

--- app/src/main/java/com/example/tresorsdeparis/MainActivity.kt
+++ app/src/main/java/com/example/tresorsdeparis/MainActivity.kt
@@ -9,16 +9,19 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
+//import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.tresorsdeparis.ui.theme.TresorsDeParisTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var webView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
+//        installSplashScreen()
         super.onCreate(savedInstanceState)
+
         setContent {
             TresorsDeParisTheme {
                 // A surface container using the 'background' color from the theme
@@ -31,30 +34,37 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
 @Composable
 fun MainContent() {
     val url = "https://tresors-de-paris.com/"
 
-    AndroidView(factory = { context ->
-        WebView(context).apply {
-            layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
-            )
-            webViewClient = WebViewClient()
-            settings.javaScriptEnabled = true
-            settings.apply {
-                // Set initial scale to 100%
-                loadWithOverviewMode = true
-                useWideViewPort = true
-                setSupportZoom(true)
-                builtInZoomControls = true
-                displayZoomControls = false
-
+        AndroidView(factory = { context ->
+            WebView(context).apply {
+                webView = this
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                webViewClient = WebViewClient()
+                settings.javaScriptEnabled = true
+                settings.apply {
+                    loadWithOverviewMode = true
+                    useWideViewPort = true
+                    setSupportZoom(true)
+                    builtInZoomControls = true
+                    displayZoomControls = false
+                }
+                loadUrl(url)
             }
-            loadUrl(url)
+        })
+    }
+
+    override fun onBackPressed() {
+        if (this::webView.isInitialized && webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
         }
-    })
+    }
 }
