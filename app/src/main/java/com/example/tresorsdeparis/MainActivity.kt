804a0dd4650913e02c6cf833bde40e50d0fce520
Valentijn <valentijnkeijser@gmail.com>
2024-06-01 17:37:22 +0200

    loading screen/facebook

--- app/src/main/java/com/example/tresorsdeparis/MainActivity.kt
+++ app/src/main/java/com/example/tresorsdeparis/MainActivity.kt
@@ -10,11 +10,13 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.tresorsdeparis.ui.theme.TresorsDeParisTheme
@@ -42,42 +44,69 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun MainContent() {
         val url = "https://tresors-de-paris.com/"
+        var isLoading by remember { mutableStateOf(true) }
 
-        AndroidView(factory = { context ->
-            WebView(context).apply {
-                webView = this
-                layoutParams = ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT
-                )
-                webViewClient = object : WebViewClient() {
-                    override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-                        val url = request?.url.toString()
-                        return if (url.startsWith("fb://")) {
-                            try {
-                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-                                context.startActivity(intent)
-                                true
-                            } catch (e: ActivityNotFoundException) {
-                                // Facebook app is not installed
-                                true // Prevent WebView from loading the URL
+        Box(modifier = Modifier.fillMaxSize()) {
+            AndroidView(factory = { context ->
+                WebView(context).apply {
+                    webView = this
+                    layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                    webViewClient = object : WebViewClient() {
+                        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                            val url = request?.url.toString()
+                            return if (url.startsWith("fb://")) {
+                                try {
+                                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                                    context.startActivity(intent)
+                                    true
+                                } catch (e: ActivityNotFoundException) {
+                                    // Facebook app is not installed
+                                    true // Prevent WebView from loading the URL
+                                }
+                            } else {
+                                false // Let WebView handle the URL
                             }
-                        } else {
-                            false // Let WebView handle the URL
+                        }
+
+                        override fun onPageFinished(view: WebView?, url: String?) {
+                            super.onPageFinished(view, url)
+                            isLoading = false
                         }
                     }
+                    settings.javaScriptEnabled = true
+                    settings.apply {
+                        loadWithOverviewMode = true
+                        useWideViewPort = true
+                        setSupportZoom(true)
+                        builtInZoomControls = true
+                        displayZoomControls = false
+                    }
+                    loadUrl(url)
                 }
-                settings.javaScriptEnabled = true
-                settings.apply {
-                    loadWithOverviewMode = true
-                    useWideViewPort = true
-                    setSupportZoom(true)
-                    builtInZoomControls = true
-                    displayZoomControls = false
+            }, modifier = Modifier.fillMaxSize())
+
+            if (isLoading) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background.copy(alpha = 0.5f)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Column {
+                        CircularProgressIndicator(
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                                .padding(bottom = 16.dp),
+                        )
+                        Text("Loading TdP ...", modifier = Modifier.align(Alignment.CenterHorizontally))
+                        Text("Your Insider's Guide to Paris")
+                    }
                 }
-                loadUrl(url)
             }
-        })
+        }
     }
 
     override fun onBackPressed() {
