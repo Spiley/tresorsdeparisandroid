b158ab709de41c44de8c719e16f7d9ca013c067a
Valentijn <valentijnkeijser@gmail.com>
2024-06-01 18:00:32 +0200

    packag name gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.tresorsdeparis"
+    namespace = "com.tresorsdeparis"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.tresorsdeparis"
+        applicationId = "com.tresorsdeparis"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
