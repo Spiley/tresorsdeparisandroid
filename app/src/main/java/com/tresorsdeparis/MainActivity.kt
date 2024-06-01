4a819dc2066e63e794a2e36d84880f731e7990f3
Valentijn <valentijnkeijser@gmail.com>
2024-06-01 17:53:51 +0200

    packagename

---
+++ app/src/main/java/com/tresorsdeparis/MainActivity.kt
@@ -0,0 +1,119 @@
+package com.tresorsdeparis
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.tresorsdeparis.ui.theme.TresorsDeParisTheme
+
+class MainActivity : ComponentActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            TresorsDeParisTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainContent()
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun MainContent() {
+        val url = "https://tresors-de-paris.com/"
+        var isLoading by remember { mutableStateOf(true) }
+
+        Box(modifier = Modifier.fillMaxSize()) {
+            AndroidView(factory = { context ->
+                WebView(context).apply {
+                    webView = this
+                    layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                    webViewClient = object : WebViewClient() {
+                        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                            val url = request?.url.toString()
+                            return if (url.startsWith("fb://")) {
+                                try {
+                                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                                    context.startActivity(intent)
+                                    true
+                                } catch (e: ActivityNotFoundException) {
+                                    // Facebook app is not installed
+                                    true // Prevent WebView from loading the URL
+                                }
+                            } else {
+                                false // Let WebView handle the URL
+                            }
+                        }
+
+                        override fun onPageFinished(view: WebView?, url: String?) {
+                            super.onPageFinished(view, url)
+                            isLoading = false
+                        }
+                    }
+                    settings.javaScriptEnabled = true
+                    settings.apply {
+                        loadWithOverviewMode = true
+                        useWideViewPort = true
+                        setSupportZoom(true)
+                        builtInZoomControls = true
+                        displayZoomControls = false
+                    }
+                    loadUrl(url)
+                }
+            }, modifier = Modifier.fillMaxSize())
+
+            if (isLoading) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background.copy(alpha = 0.5f)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Column {
+                        CircularProgressIndicator(
+                            modifier = Modifier
+                                .align(Alignment.CenterHorizontally)
+                                .padding(bottom = 16.dp),
+                        )
+                        Text("Loading TdP ...", modifier = Modifier.align(Alignment.CenterHorizontally))
+                        Text("Your Insider's Guide to Paris")
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (this::webView.isInitialized && webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
