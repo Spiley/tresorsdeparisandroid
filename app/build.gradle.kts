9ed83061822bd8dd5b42c1782d22533dc784741d
Valentijn <Valentijnkeijser@gmail.com>
2025-12-24 20:14:32 +0100

    v4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.tresorsdeparis"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
